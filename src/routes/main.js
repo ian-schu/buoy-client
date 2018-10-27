407d45a4c512181e2f54a011e8db70ab44d23a12
Ian Schumann <ian.l.schumann@gmail.com>
2018-10-27 17:38:35 -0500

    One more style cleanup on Home (for now)
    - Re-remove 'box' styling
    - Set background to plain white (temp)
    - Fine-tune vertical padding

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 const Main = () => (
 	<section class="section">
-		<div id="landing" class="box hero is-medium is-marginless">
+		<div id="landing" class="hero is-medium is-marginless">
 			<div class="hero-body">
 				<div class="container">
 					<h1 class="title has-text-brown">Put your money where your heart is.</h1>
