a712c1ffd409436ba6f7445232e44d3de2b083b4
Ian Schumann <ian.l.schumann@gmail.com>
2018-10-27 16:50:05 -0500

    Style cleanup for homepage
    - Remove white box background
    - Clean up font weights for buttons / icons
    - Prevent wrapping on icon text and buttons

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,6 @@
 const Main = () => (
 	<section class="section">
-		<div id="landing" class="hero is-medium box is-marginless">
+		<div id="landing" class="hero is-medium is-marginless">
 			<div class="hero-body">
 				<div class="container">
 					<h1 class="title has-text-brown">Put your money where your heart is.</h1>
@@ -9,26 +9,30 @@ const Main = () => (
 				<div class="hero-values">
 					<div class="hero-values__value hero-values__value--first-row has-text-teal ">
 						<i class="fas fa-store" />
-						locally owned
+						<span>locally owned</span>
 					</div>
 					<div class="hero-values__value hero-values__value--first-row has-text-gold ">
 						<i class="fas fa-coins" />
-						living wage</div>
+						<span>living wage</span>
+					</div>
 					<div class="hero-values__value hero-values__value--wide has-text-red">
 						<i class="fas fa-medal" />
-						recruits veterans</div>
+						<span>recruits veterans</span>
+					</div>
 					<div class="hero-values__value has-text-green">
 						<i class="fas fa-globe-americas" />
-						sustainable</div>
+						<span>sustainable</span>
+					</div>
 					<div class="hero-values__value has-text-purple">
 						<i class="fas fa-venus" />
-						women-owned</div>
+						<span>women-owned</span>
+					</div>
 				</div>
 			</div>
 			<div class="container">
-				<div class="buttons">
-					<button class="button is-size-5 is-danger is-outlined">Enter Zip</button>
-					<button class="button is-size-5 is-danger">Use my Location</button>
+				<div class="buttons no-wrap">
+					<button class="button has-text-weight-bold is-size-5 is-danger is-outlined">Enter ZIP</button>
+					<button class="button has-text-weight-bold is-size-5 is-danger">Use my location</button>
 				</div>
 			</div>
 		</div>
