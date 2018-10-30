fbe89d6ea23cdf10884cf1a7d1c6445fd017c0e1
Ian Schumann <ian.l.schumann@gmail.com>
2018-10-29 20:08:56 -0500

    Finish basic SearchResult styling
    - Title and subtitle text
    - Navigation arrow button at far right
    - Alignment and hover states for nav arrow

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -8,17 +8,20 @@ export default class SearchResult extends Component {
 		yelpLoading: false
 	};
 
-	render({ name, type, values }, {}) {
-		return (
-			<div class="search-result box">
-				<div>Name: {name}</div>
-				<div>Type: {type}</div>
+	render = ({ name, type, values }, {}) => (
+		<div className="search-result box">
+			<div className="search-result__left">
+				<div className="search-result__title">{name}</div>
+				<div>{type}</div>
 				<div class="search-result__values">
 					{values.map(name => (
 						<ValueIcon name={name} />
 					))}
 				</div>
 			</div>
-		);
-	}
+			<div className="search-result__right">
+				<i className="search-result__arrow far fa-arrow-alt-circle-right" />
+			</div>
+		</div>
+	);
 }
