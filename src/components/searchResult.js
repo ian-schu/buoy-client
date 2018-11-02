0209261e5ab7e09f730ead55c762d319e967ff0a
Ian Schumann <ian.l.schumann@gmail.com>
2018-11-01 21:52:47 -0500

    Address and Google Maps link on Search Result
    - Search results show simplified address (number, street, zip code)
    - Search result button WORKS and redirects to Google Maps

--- src/components/searchResult.js
+++ src/components/searchResult.js
@@ -8,20 +8,27 @@ export default class SearchResult extends Component {
 		yelpLoading: false
 	};
 
-	render = ({ data }, {}) => (
-		<div class="search-result box">
-			<div class="search-result__left">
-				<div class="search-result__title">{data.name}</div>
-				<div>{data.type}</div>
-				<div class="search-result__values">
-					{data.values.map(valueName => (
-						<ValueIcon className="search-result__value" name={valueName} />
-					))}
+	render = ({ data }, {}) => {
+		const fullAddress = [`${data.street.streetNumber} ${data.street.street}`, data.street.zip].join(
+			', ',
+		);
+
+		return (
+			<div class="search-result box">
+				<div class="search-result__left">
+					<div class="search-result__title">{data.name}</div>
+					<div>{fullAddress}</div>
+					<div>({data.type})</div>
+					<div class="search-result__values">
+						{data.values.map(valueName => (
+							<ValueIcon className="search-result__value" name={valueName} />
+						))}
+					</div>
 				</div>
+				<a href={data.mapsUrl} class="search-result__right">
+					<i class="search-result__arrow far fa-arrow-alt-circle-right" />
+				</a>
 			</div>
-			<div class="search-result__right">
-				<i class="search-result__arrow far fa-arrow-alt-circle-right" />
-			</div>
-		</div>
-	);
+		);
+	};
 }
